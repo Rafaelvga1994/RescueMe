a3a35f80cebe303b686a34226992398400789077
Adryel César <adryelcesarm@gmail.com>
2023-03-08 11:06:04 -0300

    cria componente CardShelter

--- rescue-me/src/components/pages/Dashboard/index.tsx
+++ rescue-me/src/components/pages/Dashboard/index.tsx
@@ -1,8 +1,10 @@
+import { CardShelter } from "../../CardShelter";
 
 export function Dashboard(){
     return (
         <div>
             Dashboard
+            <CardShelter imageShelter={"https://diaonline.ig.com.br/wp-content/uploads/2020/04/ongs-de-animais-em-goiania.jpg"} shelterName={"Abrigo dos Animais Refugados"} shelterCity={"Goiânia-GO"}/>
         </div>
     )
 }
