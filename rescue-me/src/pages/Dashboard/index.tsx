11686d8689020965a6bb720b6ffd15ab40c27bd8
Adryel César <adryelcesarm@gmail.com>
2023-03-08 11:40:11 -0300

    cria footer

--- rescue-me/src/pages/Dashboard/index.tsx
+++ rescue-me/src/pages/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { CardShelter } from "../../components/CardShelter"
+import { FooterPage } from "../../components/Footer"
 
 export function Dashboard() {
     return (
         <div>
             Dashboard
             <CardShelter imageShelter={"https://diaonline.ig.com.br/wp-content/uploads/2020/04/ongs-de-animais-em-goiania.jpg"} shelterName={"Abrigo dos Animais Refugados"} shelterCity={"Goiânia-GO"} />
+            <FooterPage/>
         </div>
     )
 }
--- rescue-me/src/pages/Pets/index.tsx
+++ rescue-me/src/pages/Pets/index.tsx
@@ -1,8 +1,10 @@
+import { FooterPage } from "../../components/Footer"
 
 export function PetsPage(){
     return(
         <div>
             Pets Page
+            <FooterPage/>
         </div>
     )
 }
